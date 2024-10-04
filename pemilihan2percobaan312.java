1fd2acc84148d492f4d4bf12844b914f9fb024b5
JasmineNasywa <jasminenasywa6@gmail.com>
2024-10-04 12:03:13 +0700

    Percobaan 3

--- pemilihan2percobaan312.java
+++ pemilihan2percobaan312.java
@@ -5,10 +5,36 @@ public class pemilihan2percobaan312 {
 
         String kategori;
         int penghasilan, gajibersih;
+        double pajak;
 
         System.out.println(" Masukkan kategori =");
         kategori = input12.nextLine();
-        System.out.println("Masukkan ");
+        System.out.println("Masukkan besarnya penghasilan = ");
+        penghasilan = input12.nextInt();
+
+        if (kategori.equalsIgnoreCase("pekerja")) {
+            if (penghasilan <= 2000000)
+            pajak =  0.1;
+            else if (penghasilan <= 3000000)
+            pajak = 0.15;
+            else
+            pajak = 0.2;
+
+            gajibersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasil Bersih : " + gajibersih);
+        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+            if (penghasilan <= 2500000)
+            pajak = 0.15;
+            else if (penghasilan <=3500000)
+            pajak = 0.2;
+            else
+            pajak = 0.25;
+
+            gajibersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Pengshasil bersih :" + gajibersih);
+
+        } else 
+        System.out.println("Masukkan Kategori salah.");
 
     }
 }
